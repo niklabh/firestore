7724746a7e747d74a60bd37171e128c4b24530ac
Nikhil Ranjan <niklabh811@gmail.com>
2018-07-25 01:29:15 +0200

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyBRuuAYKpNXht8zxe8kpMg0YuOwjIui6jE",
-    authDomain: "react-redux-app-6015d.firebaseapp.com",
-    databaseURL: "https://react-redux-app-6015d.firebaseio.com",
-    projectId: "react-redux-app-6015d",
-    storageBucket: "",
-    messagingSenderId: "143045144824"
+    apiKey: "AIzaSyB1uXZlDr4OaJLmbkwZ1a1IXHhskRclPoI",
+    authDomain: "heenadhingra-453c0.firebaseapp.com",
+    databaseURL: "https://heenadhingra-453c0.firebaseio.com",
+    projectId: "heenadhingra-453c0",
+    storageBucket: "heenadhingra-453c0.appspot.com",
+    messagingSenderId: "1098413749557"
 }
 
 firebase.initializeApp(config);
